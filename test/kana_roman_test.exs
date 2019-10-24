b8e9eafc6051af6fa716eb7695f7e797bc474173
nukosuke <nukosuke@users.noreply.github.com>
2019-10-24 16:31:45 +0900

    Add test for to_roman/1

--- test/kana_roman_test.exs
+++ test/kana_roman_test.exs
@@ -2,7 +2,14 @@ defmodule KanaRomanTest do
   use ExUnit.Case
   doctest KanaRoman
 
-  test "greets the world" do
-    assert KanaRoman.hello() == :world
+  describe "to_roman/1" do
+    test "convert correctly" do
+      assert {:ok, "nukosuke"} = "ヌコスケ" |> KanaRoman.to_roman()
+      assert {:ok, "tamura"} = "タムラ" |> KanaRoman.to_roman()
+      assert {:ok, "yosuke"} = "ヨウスケ" |> KanaRoman.to_roman()
+      assert {:ok, "ryosuke"} = "リョウスケ" |> KanaRoman.to_roman()
+      assert {:ok, "ando"} = "アンドウ" |> KanaRoman.to_roman()
+      assert {:ok, "hattori"} = "ハットリ" |> KanaRoman.to_roman()
+    end
   end
 end
