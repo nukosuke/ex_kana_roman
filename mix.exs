d2b16cb560d6acda098b62f6e116e0004a237460
nukosuke <nukosuke@users.noreply.github.com>
2019-10-24 16:33:06 +0900

    Fix Elixir min version

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule KanaRoman.MixProject do
     [
       app: :kana_roman,
       version: "0.0.0",
-      elixir: "~> 1.9",
+      elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
