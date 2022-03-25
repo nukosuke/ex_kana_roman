817863255eb860b1c3e3ea6dd9baf228d79ea32a
nukosuke <nukosuke@users.noreply.github.com>
2022-03-25 13:41:41 +0900

    :boom: Bump v0.1.0 :boom:

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule KanaRoman.MixProject do
   def project do
     [
       app: :kana_roman,
-      version: "0.0.0",
+      version: "0.1.0",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
       description: "カタカナ => ローマ字 変換",
